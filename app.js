deeffc204641bfc39434a532ca4c2db9a41ddd6b
seolhyelin <tosiltosilkk@gmail.com>
2022-11-09 01:16:34 +0900

    Array

--- app.js
+++ app.js
@@ -1,13 +1,42 @@
-//includes
-const isEmail = (email) => email.includes("@");
-console.log(isEmail("fdfd@fdfdf.com"));
+//Array.of
+const friends = Array.of("nico", "lin", "tosil", "hye");
 
-//repeat
-const CC_NUMBER = "6060";
-const displayName = `${"*".repeat(10)} ${CC_NUMBER}`;
-console.log(displayName);
+//Array.from
+const buttons = document.querySelectorAll("button");
+const arr = Array.from(buttons);
+arr.forEach((button) => {
+  button.addEventListener("click", () => console.log("a"));
+});
 
-//startsWith
-const myName = "Ms. hyelin";
-console.log(myName.startsWith("Ms"));
-console.log(myName.endsWith("hyelin"));
+//Array.find, Array.findIndex, Array.fill
+const emails = [
+  "hyelin@gmail.com",
+  "tosil@naver.com",
+  "yy@hotmail.com",
+  "dal@yahoo.com",
+];
+
+// const check = () => emails.findIndex((email) => email.includes("yahoo.com"));
+
+// let target = check();
+
+// if (target !== 1) {
+//   console.log(target);
+
+//   const username = emails[target].split("@")[0];
+//   const email = "korea.com";
+
+//   emails[target] = `${username}@${email}`;
+//   target = check();
+//   console.log(target);
+// }
+
+const check = () => emails.findIndex((email) => email.includes("yahoo.com"));
+
+let target = check();
+
+if (target !== 1) {
+  emails.fill("*".repeat(5), target);
+}
+
+console.log(emails);
