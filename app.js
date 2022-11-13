c703c60e8e11ae7c13ab7038d9dcaf93386241e9
seolhyelin <tosiltosilkk@gmail.com>
2022-11-13 21:08:35 +0900

    Rest + Spread + Destructure

--- app.js
+++ app.js
@@ -1,6 +1,26 @@
+//intro rest
 const best = (first, ...rest) => {
-  console.log(`my bf is ${first}`);
-  console.log(rest);
+  // console.log(`my bf is ${first}`);
+  // console.log(rest);
 };
 
 best("nic", "llin", "hye", "seol");
+
+//ex1
+const user = {
+  NAME: "nico",
+  age: 24,
+  password: 12344,
+};
+
+const killPassword = ({ password, ...rest }) => rest;
+const cleanUser = killPassword(user);
+// console.log(cleanUser);
+
+//ex2
+const setCountry = ({ country = "KR", ...rest }) => ({ country, ...rest });
+console.log(setCountry(user));
+
+//ex3
+const rename = ({ name: NAME, ...rest }) => ({ NAME, ...rest });
+console.log(rename(user));
