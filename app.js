672f0d31740742c6b96b1f3ffaa5b8422f3fc14d
seolhyelin <tosiltosilkk@gmail.com>
2022-11-13 21:28:19 +0900

    For..of

--- app.js
+++ app.js
@@ -1,26 +1,11 @@
-//intro rest
-const best = (first, ...rest) => {
-  // console.log(`my bf is ${first}`);
-  // console.log(rest);
-};
+const friends = ["g", "h", "a", "r", "3", "p", "s"];
+// const addHeart = (item, index, array) => console.log(item, index, array);
+// friends.forEach(addHeart);
 
-best("nic", "llin", "hye", "seol");
-
-//ex1
-const user = {
-  NAME: "nico",
-  age: 24,
-  password: 12344,
-};
-
-const killPassword = ({ password, ...rest }) => rest;
-const cleanUser = killPassword(user);
-// console.log(cleanUser);
-
-//ex2
-const setCountry = ({ country = "KR", ...rest }) => ({ country, ...rest });
-console.log(setCountry(user));
-
-//ex3
-const rename = ({ name: NAME, ...rest }) => ({ NAME, ...rest });
-console.log(rename(user));
+for (const friend of friends) {
+  if (friend === "p") {
+    break;
+  } else {
+    console.log(friend);
+  }
+}
