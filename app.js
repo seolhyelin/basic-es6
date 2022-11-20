b2b38cccf642424a31acb8f7bb635da847ebc412
seolhyelin <tosiltosilkk@gmail.com>
2022-11-20 21:31:41 +0900

    Promise.all

--- app.js
+++ app.js
@@ -1,17 +1,14 @@
-const amIcute = new Promise((resolve, reject) => {
-  resolve(3);
+const p1 = new Promise((res) => {
+  setTimeout(res, 10000, "fist");
+});
+const p2 = new Promise((res, reject) => {
+  setTimeout(reject, 1000, "hate JS");
+});
+const p3 = new Promise((res) => {
+  setTimeout(res, 4000, "third");
 });
 
-const timesThird = (n) => n * 2;
-
-amIcute
-  .then(timesThird)
-  .then(timesThird)
-  .then(timesThird)
-  .then(timesThird)
-  .then(timesThird)
-  .then(() => {
-    throw Error("wrong!");
-  })
-  .then((lastNumber) => console.log(lastNumber))
+const motherPromise = Promise.all([p1, p2, p3]);
+motherPromise
+  .then((values) => console.log(values))
   .catch((err) => console.log(err));
