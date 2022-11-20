22e76423931d05674b07109c9a9f10fabe71b2f8
seolhyelin <tosiltosilkk@gmail.com>
2022-11-20 21:52:08 +0900

    finally

--- app.js
+++ app.js
@@ -1,13 +1,6 @@
-const p1 = new Promise((res) => {
-  setTimeout(res, 10000, "fist");
-});
-const p2 = new Promise((res, reject) => {
-  setTimeout(reject, 5000, "hate JS");
-});
-const p3 = new Promise((res) => {
-  setTimeout(res, 3000, "third");
-});
-
-Promise.race([p1, p2, p3])
-  .then((values) => console.log(values))
-  .catch((err) => console.log(err));
+const p1 = new Promise((res, reject) => {
+  setTimeout(reject, 4000, "fist");
+})
+  .then((value) => console.log(value))
+  .catch(() => console.log("err"))
+  .finally(() => console.log("im done"));
