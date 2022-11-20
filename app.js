e9d4ad951041ba39d2a820626cb9c4bad4782b02
seolhyelin <tosiltosilkk@gmail.com>
2022-11-20 23:06:36 +0900

    try catch finally

--- app.js
+++ app.js
@@ -1,18 +1,14 @@
-const getMoviesPromise = () => {
-  fetch("https://yts.mx/api/v2/list_movies.json")
-    .then((res) => {
-      console.log(res);
-      return res.json();
-    })
-    .then((json) => console.log(json))
-    .catch((e) => console.log(`🔥${e}`));
-};
-
 const getMoviesAsync = async () => {
-  const response = await fetch("https://yts.mx/api/v2/list_movies.json");
-  console.log(response);
-  const json = await response.json();
-  console.log(json);
+  try {
+    const response = await fetch("https://yts.mx/api/v2/list_movies.json");
+    const json = await response.json();
+    console.log(json);
+    // throw Error("im hungry");
+  } catch (e) {
+    console.log(e);
+  } finally {
+    console.log("we are done");
+  }
 };
 
 getMoviesAsync();
