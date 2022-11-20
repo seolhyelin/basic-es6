f4a47cc8fa7090a2dbac3b143911e22f0b979eef
seolhyelin <tosiltosilkk@gmail.com>
2022-11-20 21:45:44 +0900

    Promise.race

--- app.js
+++ app.js
@@ -2,13 +2,12 @@ const p1 = new Promise((res) => {
   setTimeout(res, 10000, "fist");
 });
 const p2 = new Promise((res, reject) => {
-  setTimeout(reject, 1000, "hate JS");
+  setTimeout(reject, 5000, "hate JS");
 });
 const p3 = new Promise((res) => {
-  setTimeout(res, 4000, "third");
+  setTimeout(res, 3000, "third");
 });
 
-const motherPromise = Promise.all([p1, p2, p3]);
-motherPromise
+Promise.race([p1, p2, p3])
   .then((values) => console.log(values))
   .catch((err) => console.log(err));
