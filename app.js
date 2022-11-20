9aa26151bfc90e976715acf05914b6a529518447
seolhyelin <tosiltosilkk@gmail.com>
2022-11-20 22:40:20 +0900

    Async Await

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
-fetch("https://yts.torrentbay.to/api/v2/list_movies.json")
-  .then((res) => {
-    console.log(res);
-    return res.json();
-  })
-  .then((json) => console.log(json))
-  .catch((e) => console.log(`🔥${e}`));
+const getMoviesPromise = () => {
+  fetch("https://yts.mx/api/v2/list_movies.json")
+    .then((res) => {
+      console.log(res);
+      return res.json();
+    })
+    .then((json) => console.log(json))
+    .catch((e) => console.log(`🔥${e}`));
+};
+
+const getMoviesAsync = async () => {
+  const response = await fetch("https://yts.mx/api/v2/list_movies.json");
+  console.log(response);
+  const json = await response.json();
+  console.log(json);
+};
+
+getMoviesAsync();
