a2dff3d26dd2f548d5032b64657740d5b89d3398
seolhyelin <tosiltosilkk@gmail.com>
2022-11-28 22:35:58 +0900

    Extending Classes

--- app.js
+++ app.js
@@ -1,19 +1,33 @@
 class User {
-  constructor(name) {
+  constructor(name, lastName, email, password) {
     this.username = name;
+    this.lastName = lastName;
+    this.email = email;
+    this.password = password;
   }
   sayHello() {
     console.log(`hello ${this.username}`);
   }
+  getProfile() {
+    console.log(`${this.username} ${this.email} ${this.password}`);
+  }
+  updatePassword(newPassword, currentPassword) {
+    if (currentPassword === this.password) {
+      this.password = newPassword;
+    } else {
+      console.log("can't change");
+    }
+  }
 }
 
-// const baseObject = {
-//   username: "hyelin",
-//   sayHello: function () {
-//     console.log("hyelin~~~~");
-//   },
-// };
+const user = new User("lin", "hye", "asd@com", "1234");
 
-const user = new User("lin");
-console.log(user.username);
-user.sayHello();
+class Admin extends User {
+  deleteWebsite() {
+    console.log("deleting....");
+  }
+}
+
+const admin = new Admin("lin", "hye", "asd@com", "1234");
+admin.deleteWebsite();
+console.log(admin.email);
